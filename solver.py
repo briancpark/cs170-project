677b3f36ba8518d06e866bf79d865a469eb30a89
Brian Park <briancpark@berkeley.edu>
2020-11-21 23:40:39 -0500

    fixed a few bugs. Brute force fully working with solve.py script

--- solver.py
+++ solver.py
@@ -2,7 +2,8 @@ import networkx as nx
 from parse import read_input_file, write_output_file
 from utils import is_valid_solution, calculate_happiness
 import sys
-
+import utils
+import parse
 
 
 
@@ -15,9 +16,69 @@ def solve(G, s):
         D: Dictionary mapping for student to breakout room r e.g. {0:2, 1:0, 2:1, 3:2}
         k: Number of breakout rooms
     """
+    cliques = list(nx.enumerate_all_cliques(G))
 
-    # TODO: your code here!
-    pass
+    rooms_meta_data = {}
+
+    for clique in cliques:
+        stress = utils.calculate_stress_for_room(clique, G)
+        happiness = utils.calculate_happiness_for_room(clique, G)
+        rooms_meta_data[frozenset(clique)] = [stress, happiness]
+
+    nodes = [node for node in G.nodes]
+
+    partioned_sets = []
+
+    for n, p in enumerate(partition(nodes), 1):
+        partioned_sets.append(sorted(p))
+
+    partioned_sets_data = []
+    for sets in partioned_sets:
+        total_happiness = 0
+        total_stress = 0
+        for part in sets:
+            total_happiness += rooms_meta_data[frozenset(part)][0]
+            total_stress += rooms_meta_data[frozenset(part)][1]
+
+        partioned_sets_data.append((sets, total_happiness, total_stress))
+
+    deletion = []
+
+    for partitioned_room in partioned_sets_data:
+        for i in range(len(partitioned_room[0])):
+            if s / len(partitioned_room[0]) < utils.calculate_stress_for_room(partitioned_room[0][i], G):
+                deletion.append(partitioned_room)
+                continue
+            
+    for rem in deletion:
+        if rem in partioned_sets_data:
+            partioned_sets_data.remove(rem)
+
+    partioned_sets_data.sort(key=lambda x:x[2], reverse=True)
+    optimal = partioned_sets_data[0]
+
+    d = {}
+
+    for i in range(len(optimal[0])):
+        for j in optimal[0][i]:
+            d[j] = i
+
+    return d, len(optimal[0])
+
+def partition(collection):
+    if len(collection) == 1:
+        yield [ collection ]
+        return
+
+    first = collection[0]
+    for smaller in partition(collection[1:]):
+        # insert `first` in each of the subpartition's subsets
+        for n, subset in enumerate(smaller):
+            yield smaller[:n] + [[ first ] + subset]  + smaller[n+1:]
+        # put `first` in its own subset 
+        yield [ [ first ] ] + smaller
+
+
 
 def brute_force_solve(G, s):
     """
@@ -140,21 +201,22 @@ def enumerate_all_cliques(G):
 
 # Usage: python3 solver.py test.in
 
-# if __name__ == '__main__':
-#     assert len(sys.argv) == 2
-#     path = sys.argv[1]
-#     G, s = read_input_file(path)
-#     D, k = solve(G, s)
-#     assert is_valid_solution(D, G, s, k)
-#     print("Total Happiness: {}".format(calculate_happiness(D, G)))
-#     write_output_file(D, 'out/test.out')
+if __name__ == '__main__':
+    assert len(sys.argv) == 2
+    path = sys.argv[1]
+    G, s = read_input_file(path)
+    D, k = solve(G, s)
+    assert is_valid_solution(D, G, s, k)
+    print("Total Happiness: {}".format(calculate_happiness(D, G)))
+    write_output_file(D, 'out/test.out')
 
-
-# For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
+# import glob
+# from os.path import basename, normpath
+# # For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
 # if __name__ == '__main__':
-#     inputs = glob.glob('file_path/inputs/*')
+#     inputs = glob.glob('in_out/*')
 #     for input_path in inputs:
-#         output_path = 'file_path/outputs/' + basename(normpath(input_path))[:-3] + '.out'
+#         output_path = 'out/' + basename(normpath(input_path))[:-3] + '.out'
 #         G, s = read_input_file(input_path, 100)
 #         D, k = solve(G, s)
 #         assert is_valid_solution(D, G, s, k)
