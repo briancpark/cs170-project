ea4a8ba704aad9a3be23760f6a89f45fde456a63
Brian Park <briancpark@berkeley.edu>
2020-12-07 20:35:12 -0500

    update project

--- solver.py
+++ solver.py
@@ -2,12 +2,17 @@ import networkx as nx
 from parse import read_input_file, write_output_file
 from utils import is_valid_solution, calculate_happiness
 import sys
-<<<<<<< HEAD
 import utils
 import parse
 import itertools
 from os.path import basename, normpath
 import glob
+
+
+"""
+    Documentation: hhttps://networkx.org/documentation/stable/index.html
+
+"""
 
 
 def solve(G, s):
@@ -19,125 +24,132 @@ def solve(G, s):
         D: Dictionary mapping for student to breakout room r e.g. {0:2, 1:0, 2:1, 3:2}
         k: Number of breakout rooms
     """
-
+    # If inputs are small, we run our naive method.
     if len(G.nodes) == 10:
-        cliques = list(nx.enumerate_all_cliques(G))
-
-        rooms_meta_data = {}
-
-        for clique in cliques:
-            stress = utils.calculate_stress_for_room(clique, G)
-            happiness = utils.calculate_happiness_for_room(clique, G)
-            rooms_meta_data[frozenset(clique)] = [stress, happiness]
-
-        nodes = [node for node in G.nodes]
-
-        partioned_sets = []
-
-        for n, p in enumerate(partition(nodes), 1):
-            partioned_sets.append(sorted(p))
-
-        partioned_sets_data = []
-        for sets in partioned_sets:
-            total_happiness = 0
-            total_stress = 0
-            for part in sets:
-                total_happiness += rooms_meta_data[frozenset(part)][0]
-                total_stress += rooms_meta_data[frozenset(part)][1]
-
-            partioned_sets_data.append((sets, total_happiness, total_stress))
-
-        deletion = []
-
-        for partitioned_room in partioned_sets_data:
-            for i in range(len(partitioned_room[0])):
-                if s / len(partitioned_room[0]) < utils.calculate_stress_for_room(partitioned_room[0][i], G):
-                    deletion.append(partitioned_room)
-                    continue
-                
-        for rem in deletion:
-            if rem in partioned_sets_data:
-                partioned_sets_data.remove(rem)
-
-        partioned_sets_data.sort(key=lambda x:x[2], reverse=True)
-        optimal = partioned_sets_data[0]
-
-        d = {}
-
-        for i in range(len(optimal[0])):
-            for j in optimal[0][i]:
-                d[j] = i
-
-        return d, len(optimal[0])
-    else:
-
-        if len(G.nodes) == 20:
-            limit = s / 7
-        else:
-            limit = s / 17
-
-        triple_cliques = list(itertools.combinations(G, 3))
-
-        rooms_meta_data = {}
-
-        for clique in triple_cliques:
-            stress = utils.calculate_stress_for_room(clique, G)
-            happiness = utils.calculate_happiness_for_room(clique, G)
-            rooms_meta_data[frozenset(clique)] = [stress, happiness]
-
-        deletion = []
-
-        for key in rooms_meta_data:
-            if rooms_meta_data[key][0] > limit:
-                deletion.append(key)
-            
-        for key in deletion:
-            del rooms_meta_data[key]
-
-        nodes = [node for node in G.nodes]
-
-
-        combined_set = [] # this contains our answer
-        seen_sets = [] #add seen here
-        i = 0
-
-        #This becomes the triple clique converted list
-        for vertices in rooms_meta_data.keys():
-            breakTrue = False
-            for vertex in list(vertices):
-                if vertex in seen_sets:
-                    breakTrue = True
-
-            if breakTrue:
+        return naive(G,s)
+    
+    else: #For medium/large inputs
+        return tripleClique(G,s)
+    
+"""
+    Brute Force method used for small inputs.
+    Main idea: Try all combinations.
+    Runtime: Very long.
+    Note: This does not work for medium/large.
+"""
+def naive(G, s):
+    # If the size is 10, then we use brute force method.
+    
+    cliques = list(nx.enumerate_all_cliques(G))
+    rooms_meta_data = {}
+    for clique in cliques:
+        stress = utils.calculate_stress_for_room(clique, G)
+        happiness = utils.calculate_happiness_for_room(clique, G)
+        rooms_meta_data[frozenset(clique)] = [stress, happiness]
+    nodes = [node for node in G.nodes]
+    partioned_sets = []
+    for n, p in enumerate(partition(nodes), 1):
+        partioned_sets.append(sorted(p))
+    partioned_sets_data = []
+    for sets in partioned_sets:
+        total_happiness = 0
+        total_stress = 0
+        for part in sets:
+            total_happiness += rooms_meta_data[frozenset(part)][0]
+            total_stress += rooms_meta_data[frozenset(part)][1]
+        partioned_sets_data.append((sets, total_happiness, total_stress))
+    deletion = []
+    for partitioned_room in partioned_sets_data:
+        for i in range(len(partitioned_room[0])):
+            if s / len(partitioned_room[0]) < utils.calculate_stress_for_room(partitioned_room[0][i], G):
+                deletion.append(partitioned_room)
                 continue
+    for rem in deletion:
+        if rem in partioned_sets_data:
+            partioned_sets_data.remove(rem)
 
-            combined_set.append(vertices)
-            for seen_v in list(vertices):
-                seen_sets.append(seen_v)
+    partioned_sets_data.sort(key=lambda x:x[2], reverse=True)
+    optimal = partioned_sets_data[0]
 
-        duo_clique = [x for x in nodes if x not in seen_sets]
+    d = {}
 
-        duo_clique
-        utils.calculate_stress_for_room(duo_clique, G)
+    for i in range(len(optimal[0])):
+        for j in optimal[0][i]:
+            d[j] = i
 
-        combined_set_list = []
+    return d, len(optimal[0])
 
 
-        for frozen_set in combined_set:
-            combined_set_list.append(list(frozen_set))
+"""
+    Triple Clique method used for medium and large inputs.
+    Main idea: 
+    Runtime:
+"""
+def tripleClique(G,s):
+    if len(G.nodes) == 20:
+        limit = s / 7
+    else:
+        limit = s / 17
 
-        combined_set_list.append(duo_clique)
-        combined_set_list
+    triple_cliques = list(itertools.combinations(G, 3))
 
-        d = {}
-        i = 0
+    rooms_meta_data = {}
 
-        for clique in combined_set_list:
-            for j in clique:
-                d[j] = i
-            i += 1
+    for clique in triple_cliques:
+        stress = utils.calculate_stress_for_room(clique, G)
+        happiness = utils.calculate_happiness_for_room(clique, G)
+        rooms_meta_data[frozenset(clique)] = [stress, happiness]
 
-        return d, len(combined_set_list)
+    deletion = []
+
+    for key in rooms_meta_data:
+        if rooms_meta_data[key][0] > limit:
+            deletion.append(key)
+        
+    for key in deletion:
+        del rooms_meta_data[key]
+
+    nodes = [node for node in G.nodes]
+    combined_set = [] # this contains our answer
+    seen_sets = [] #add seen here
+    i = 0
+    #This becomes the triple clique converted list
+    for vertices in rooms_meta_data.keys():
+        breakTrue = False
+        for vertex in list(vertices):
+            if vertex in seen_sets:
+                breakTrue = True
+        if breakTrue:
+            continue
+        combined_set.append(vertices)
+        for seen_v in list(vertices):
+            seen_sets.append(seen_v)
+    duo_cliques = [x for x in nodes if x not in seen_sets]
+    duo_cliques
+    #utils.calculate_stress_for_room(duo_cliques, G)
+    combined_set_list = []
+    for frozen_set in combined_set:
+        combined_set_list.append(list(frozen_set))
+    ### TODO:
+    if len(duo_cliques) > 2:
+        print(len(duo_cliques))
+        #if there more than 2 left over, asign them to each room.
+        for duo_clique in duo_cliques:
+            combined_set_list.append([duo_clique])
+    else:
+        #Fill it up with duoclique of 2 nodes
+        combined_set_list.append(duo_cliques)
+    ### TODO:
+    combined_set_list
+    d = {}
+    i = 0
+
+    for clique in combined_set_list:
+        for j in clique:
+            d[j] = i
+        i += 1
+
+    return d, len(combined_set_list)
 
 
 def partition(collection):
@@ -173,15 +185,22 @@ from os.path import basename, normpath
 # For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
 if __name__ == '__main__':
     #inputs = glob.glob('inputs/*')
-    inputs = glob.glob('inputs/small/*.in')
+    inputs = glob.glob('inputs/medium/*.in')
+    total = 0
     for input_path in inputs:
-        output_path = 'outputs/small/' + basename(normpath(input_path))[:-3] + '.out'
+        output_path = 'outputs/medium/' + basename(normpath(input_path))[:-3] + '.out'
         G, s = read_input_file(input_path, 100)
         D, k = solve(G, s)
-        assert is_valid_solution(D, G, s, k)
-        #cost_t = calculate_happiness(T)
-        write_output_file(D, output_path)
+        try:
+            assert is_valid_solution(D, G, s, k)
+            write_output_file(D, output_path)
+            print("success: " + str(input_path))
+            total += 1
+        except AssertionError as error:
+            print("invalid: " + str(input_path))
 
+        #cost_t = calculate_happiness(T)
+    print(total / len(inputs))
 # if __name__ == '__main__':
 #     inputs = glob.glob('inputs/*')
 #     for input_path in inputs:
